54e40e65a146543cb2aabe358700d88a7ab5deb5
Mr.Susheel Kumar <74233895+Susheel4115@users.noreply.github.com>
2022-05-13 21:36:48 +0530

    Final commit
    Ready for build and deployment.

---
+++ index.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
+app.use(express.urlencoded({ extended: true }));
+const port = process.env.PORT;
+const TodoTask = require("./models/TodoTask");
+mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true }, () => {});
+
+app.set("view engine", "ejs");
+
+app.listen(port, () => {
+  console.log(`Server is running and listening to port ${port}`);
+});
+
+// GET METHOD
+app.get("/", (req, res) => {
+  TodoTask.find({}, (err, tasks) => {
+    res.render("todo.ejs", { todoTasks: tasks });
+  });
+});
+
+app.post("/", async (req, res) => {
+  const todoTask = new TodoTask({
+    content: req.body.content,
+  });
+  console.log(todoTask);
+  try {
+    await todoTask.save();
+    res.redirect("/");
+  } catch (err) {
+    res.redirect("/");
+  }
+});
+
+//UPDATE
+app
+  .route("/edit/:id")
+  .get((req, res) => {
+    const id = req.params.id;
+    TodoTask.find({}, (err, tasks) => {
+      res.render("todoEdit.ejs", { todoTasks: tasks, idTask: id });
+    });
+  })
+  .post((req, res) => {
+    const id = req.params.id;
+    TodoTask.findByIdAndUpdate(id, { content: req.body.content }, (err) => {
+      if (err) return res.send(500, err);
+      res.redirect("/");
+    });
+  });
+
+//delete
+app.route("/remove/:id").get((req, res) => {
+  const id = req.params.id;
+  TodoTask.findByIdAndRemove(id, (err) => {
+    if (err) return res.send(500, err);
+    res.redirect("/");
+  });
+});
